60f7c64636abe4c9a90fd90198800a489cda6394
Biuni <info.gianlucabonifazi@gmail.com>
2020-04-20 12:12:22 +0200

    Add 03-31-2020

--- script/generate_json.py
+++ script/generate_json.py
@@ -7,15 +7,17 @@ JSONfiles = glob.glob('../data/JSON/*.json')
 data = {}
 data['report'] = []
 
-def generate(out=True):
-	for json_file in JSONfiles:
-		print('Inserito: '+ os.path.basename(json_file)) if out else None
-		with open(json_file) as file:
-			data['report'].append(json.load(file))
 
-	with open('../covid-19-marche.json', 'w') as outfile:
-			json.dump(data, outfile, indent=2)
-			print('\nFile "covid-19-marche.json" generato!')
+def generate(out=True):
+    for json_file in JSONfiles:
+        print('Inserito: ' + os.path.basename(json_file)) if out else None
+        with open(json_file) as file:
+            data['report'].append(json.load(file))
+
+    with open('../covid-19-marche.json', 'w') as outfile:
+        json.dump(data, outfile, indent=2)
+        print('\nFile "covid-19-marche.json" generato!')
+
 
 if __name__ == '__main__':
-	generate()
+    generate()
