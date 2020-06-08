c77542af14da022fae66be79e44cac86be2690ee
Biuni <info.gianlucabonifazi@gmail.com>
2020-06-08 10:10:11 +0200

    Add 06-07-2020

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 </p>
 
 <p align="center">
-📅 <i>Dati aggiornati al 06/06/2020</i> 📅
+📅 <i>Dati aggiornati al 07/06/2020</i> 📅
 </p>
 
 # 🧪 Regione Marche - Dati COVID-19 in CSV e JSON
--- script/download_pdf.py
+++ script/download_pdf.py
@@ -12,6 +12,7 @@ class bcolors:
 
 
 gores_blu = (
+    ("06-07-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORESblu07062020_9.pdf"),
     ("06-06-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORESblu06062020_9.pdf"),
     ("06-05-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORESblu05062020_9.pdf"),
     ("06-04-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORESblu04062020_9.pdf"),
@@ -102,6 +103,7 @@ gores_blu = (
 )
 
 gores_giallo = (
+    ("06-07-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Goresgialla07062020.pdf"),
     ("06-06-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Goresgialla06062020.pdf"),
     ("06-05-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/goresgiallo5giugno.pdf"),
     ("06-04-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/goresgiallo4giugno.pdf"),
@@ -199,6 +201,7 @@ gores_giallo = (
 )
 
 gores_arancio = (
+    ("06-07-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/goresarancio07062020.pdf"),
     ("06-06-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/goresarancio06062020.pdf"),
     ("06-05-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Report_sint_decessi_COVID19_agg_5giugno2020_ore18.pdf"),
     ("06-04-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORESarancio04062020_18.pdf"),
