be54b4245178a35e9b7406b5005f9f6ce95257b0
Biuni <info.gianlucabonifazi@gmail.com>
2020-09-25 21:32:38 +0200

    Add 09-24-2020

--- script/download_pdf.py
+++ script/download_pdf.py
@@ -12,6 +12,7 @@ class bcolors:
 
 
 gores_blu = (
+    ("09-24-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORES%20BLU%2024092020%20ore%209%20revAS.pdf"),
     ("09-23-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORES%20BLU%2023092020%20ore%209%20revAS.pdf"),
     ("09-22-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORES%20BLU%2022092020%20ore%209%20revAS.pdf"),
     ("09-21-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORES%20BLU%2021092020%20ore%209%20revAS.pdf"),
@@ -211,6 +212,7 @@ gores_blu = (
 )
 
 gores_giallo = (
+    ("09-24-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20gialla%2024092020%20ore%2012.pdf"),
     ("09-23-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20gialla%2023092020%20ore%2012.pdf"),
     ("09-22-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20gialla%2022092020%20ore%2012.pdf"),
     ("09-21-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20gialla%2021092020%20ore%2012.pdf"),
@@ -417,6 +419,7 @@ gores_giallo = (
 )
 
 gores_arancio = (
+    ("09-24-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20arancio%2024092020%20ore%2018.pdf"),
     ("09-23-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20arancio%2023092020%20ore%2018.pdf"),
     ("09-22-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20arancio%2022092020%20ore%2018.pdf"),
     ("09-21-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20arancio%2021092020%20ore%2018.pdf"),
