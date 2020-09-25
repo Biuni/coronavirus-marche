6f53e0459725faf9014e84092c4573e14a972415
Biuni <info.gianlucabonifazi@gmail.com>
2020-09-25 20:43:34 +0200

    Add 09-17-2020

--- script/download_pdf.py
+++ script/download_pdf.py
@@ -12,6 +12,7 @@ class bcolors:
 
 
 gores_blu = (
+    ("09-17-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORES%20BLU%2017092020%20ore%209%20revAS.pdf"),
     ("09-16-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORES%20BLU%2016092020%20ore%209%20revAS.pdf"),
     ("09-15-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORES%20BLU%2015092020%20ore%209%20revAS.pdf"),
     ("09-14-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/GORES%20BLU%2014092020%20ore%209%20revAS.pdf"),
@@ -204,6 +205,7 @@ gores_blu = (
 )
 
 gores_giallo = (
+    ("09-17-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/2_report_sintetico_monitoraggio_COVID19_agg_17_settembre_2020.pdf"),
     ("09-16-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20gialla%2016092020%20ore%2012.pdf"),
     ("09-15-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20gialla%2015092020%20ore%2012.pdf"),
     ("09-14-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20gialla%2014092020%20ore%2012.pdf"),
@@ -403,6 +405,7 @@ gores_giallo = (
 )
 
 gores_arancio = (
+    ("09-17-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20arancio%2017092020%20ore%2018.pdf"),
     ("09-16-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20arancio%2016092020%20ore%2018.pdf"),
     ("09-15-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20arancio%2015092020%20ore%2018.pdf"),
     ("09-14-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gores%20arancio%2014092020%20ore%2018.pdf"),
