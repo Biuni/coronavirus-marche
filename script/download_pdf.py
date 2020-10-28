af423579e7bf70758efd02b2c1c42243668cb211
Biuni <info.gianlucabonifazi@gmail.com>
2020-10-28 20:32:32 +0100

    Add 10-27-2020

--- script/download_pdf.py
+++ script/download_pdf.py
@@ -12,6 +12,7 @@ class bcolors:
 
 
 gores_blu = (
+    ("10-27-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/BLU%2027102020%20ore%209%20revAS.pdf"),
     ("10-26-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/BLU%2026102020%20ore%209%20revAS.pdf"),
     ("10-25-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/BLU%2025102020%20ore%209%20revAS.pdf"),
     ("10-24-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/BLU%2024102020%20ore%209%20revAS.pdf"),
@@ -244,6 +245,7 @@ gores_blu = (
 )
 
 gores_giallo = (
+    ("10-27-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gialla%2027102020%20ore%2012.pdf"),
     ("10-26-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gialla%2026102020%20ore%2012.pdf"),
     ("10-25-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gialla%2025102020%20ore%2012.pdf"),
     ("10-24-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Gialla%2024102020%20ore%2012.pdf"),
@@ -483,6 +485,8 @@ gores_giallo = (
 )
 
 gores_arancio = (
+    ("10-27-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Arancio%2027102020%20ore%2018.pdf"),
+    ("10-27-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Arancio%2027102020%20ore%2018.pdf"),
     ("10-26-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Arancio%2026102020%20ore%2018.pdf"),
     ("10-25-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Arancio%2025102020%20ore%2018.pdf"),
     ("10-24-2020.pdf", "https://www.regione.marche.it/portals/0/Salute/CORONAVIRUS/DatiGORES/Arancio%2024102020%20ore%2018.pdf"),
